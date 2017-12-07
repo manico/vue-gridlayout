245f0f87dd6a1755a2f81465ae0ac3dbf7ad600d
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2017-12-07 23:23:58 +0100

    Update example

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,27 @@
     <v-grid class="grid"
             template-columns="2fr 1fr 1fr"
             :auto-rows="['50px', '120px']"
-            :template-areas="['a b', 'a b']"
             gap="10px">
       <v-grid-item :column-start="1"
                    :column-end="4">
-        One
       </v-grid-item>
-      <v-grid-item>Two</v-grid-item>
-      <v-grid-item>Three</v-grid-item>
-      <v-grid-item>Four</v-grid-item>
-      <v-grid-item>Five</v-grid-item>
+      <v-grid-item></v-grid-item>
+      <v-grid-item></v-grid-item>
+      <v-grid-item></v-grid-item>
+      <v-grid-item :column-start="1"
+                   :column-end="4">
+      </v-grid-item>
+    </v-grid>
+    <v-grid class="grid"
+            template-columns="1fr 1fr 1fr"
+            auto-rows="100px"
+            :template-areas="['a a b', 'a a b']"
+            gap="10px">
+      <v-grid-item area="a"></v-grid-item>
+      <v-grid-item></v-grid-item>
+      <v-grid-item></v-grid-item>
+      <v-grid-item></v-grid-item>
+      <v-grid-item area="b"></v-grid-item>
     </v-grid>
   </div>
 </template>
